0047486ab57a51cf4f47bb32a5f264434edbc2d5
Nguyen Thanh Lan <lannt@rikkeisoft.com>
2019-09-22 15:14:47 +0700

    pjt initial

---
+++ src/main/java/xyz/lannt/truyencvcollector/TruyencvCollectorApplication.java
@@ -0,0 +1,13 @@
+package xyz.lannt.truyencvcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruyencvCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruyencvCollectorApplication.class, args);
+	}
+
+}
